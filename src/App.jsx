600e83d3054ed21abd9a14da9de94705691acf3e
Ayushaxel <arjunkumar18526@gmail.com>
2024-10-19 12:56:38 +0530

    task20

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './component/header'
+import Main from './component/main'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return(
+    <>
+    <nav>
+        <div className="logo">Logo</div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    
+    </>
+    )
+}
+export default  Header
